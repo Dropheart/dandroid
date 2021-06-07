75d55ba5018fcfcf822e6aaced5b83edf7c2fcc6
Dropheart <ahmed@techfromtheheart.com>
2021-06-07 12:17:31 +0300

    we are an ID server not a PS server

--- newsfeed/tempreddit.js
+++ newsfeed/tempreddit.js
@@ -4,7 +4,7 @@ const feeder = new rssfeeder({ skipFirstLoad: true });
 module.exports = async (client) => {
     client.once('ready', async () => {
         feeder.add({
-            url: 'http://feeds.feedburner.com/psblog',
+            url: 'https://www.reddit.com/r/imaginedragons/new.rss',
         })
         
         const blog = await client.guilds.resolve('226576492782551042').channels.resolve('294453624543903744')
