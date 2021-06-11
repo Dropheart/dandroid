8981c39b5dd22471aa0e60c3236d8b0f8060beda
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2021-06-11 03:17:32 +0300

    get h0nda too

--- listeners/guildMemberAdd.js
+++ listeners/guildMemberAdd.js
@@ -9,7 +9,7 @@ class guildMemberAddListener extends Listener {
     }
     
     exec(member) {
-        if (member.user.username.toLowerCase().includes('h0nde')) member.ban({ days: 7, reason: 'Automatic ban for h0nde spambot accounts.' })
+        if (member.user.username.toLowerCase().includes('h0nde') || member.user.username.toLowerCase().includes('h0nda')) member.ban({ days: 7, reason: 'Automatic ban for h0nde spambot accounts.' })
     }
 }
 
