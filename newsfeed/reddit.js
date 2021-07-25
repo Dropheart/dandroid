d8516482fa4f530ff9cde0bfeba19ecb2495abc0
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2021-07-25 22:59:38 +0200

    Rebuild Embeds for Reddit Posts

--- newsfeed/reddit.js
+++ newsfeed/reddit.js
@@ -16,42 +16,35 @@ module.exports = async (client) => {
 
     const submissions = new SubmissionStream(wrapper, {
         subreddit: "imaginedragons",
-        limit: 1,
+        limit: 0,
         pollTime: 2000,
     });
 
-    submissions.on("item", console.log);
+    //submissions.on("item", console.log);
 
-    // ToDo: Change embed Builder to work with Snoostorm
-    /*submissions.on('item', function(post) {
-        if (url == post.data.url) {
-            return;
-        }
-        url = post.data.url 
+    submissions.on("item", function(post) {
+        url = "https://reddit.com" + post.permalink;
+
         embed = new MessageEmbed()
-        .setTitle(post.data.title)
-        .setURL(`https://reddit.com${post.data.permalink}`)
-        .setColor('ff4500')
-        .setTitle(`Posted by u/${post.data.author}`)
-        .setTimestamp()
-        
-        if (post.data.post_hint == 'image') {
-            embed.addField(post.data.title, post.data.url)
-            embed.setImage(post.data.url)
-            console.log('1')
-        } else if (post.data.post_hint) {
-            embed.addField(post.data.title, post.data.url)
-            console.log('4')
-        } else if (post.data.selftext) {
-            embed.addField(post.data.title, post.data.selftext)
-            console.log('2')
+        .setTitle(post.title)
+        .setURL(url)
+        .setColor("ff4500")
+        .setTitle("Posted by u/" + post.author.name)
+        .setTimestamp();
+
+        if (post.post_hint == 'image') {
+            embed.addField(post.title, post.url)
+            embed.setImage(post.url)
+        } else if (post.post_hint) {
+            embed.addField(post.title, post.url)
+        } else if (post.selftext) {
+            embed.addField(post.title, post.selftext)
         } else {
-            embed.addField(post.data.title, '_No selftext was provided_')
-            console.log('3')
+            embed.addField(post.title, '_No selftext was provided_')
         }
 
-        client.channels.cache.get(config.redditChannel).send(embed)
 
+        client.channels.cache.get(config.redditChannel).send(embed);
     })
-    .on('error', console.error)*/
+    .on('error', console.error);
 }
