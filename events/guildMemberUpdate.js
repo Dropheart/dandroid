32edcce5f25da6179521810530967d14354c155a
Dropheart <ahmed@techfromtheheart.com>
2021-10-11 02:59:18 +0300

    dandroid welcome messages, only once accepted rules

---
+++ events/guildMemberUpdate.js
@@ -0,0 +1,6 @@
+module.exports = async (client, oldMember, newMember) => {
+    if (oldMember.pending && !newMember.pending) {
+        welcomeChannel = await client.channels.fetch(config.welcomeChannel);
+        welcomeChannel.send(`Welcome <@${newMember.id}> to the Imagine Dragons server! Feel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself. If you want roles / colored names, type "-rr" for the role selection. We hope you enjoy your stay here!`)
+    }
+}
