020eab7adc2d543d42ae2ab904b1a11c567fc131
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2021-10-12 11:35:25 +0200

    Fix Welcome Message Wording

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,7 +2,7 @@ module.exports = async (client, oldMember, newMember) => {
     if (newMember.guild.id == "226576492782551042") {
         if (oldMember.pending && !newMember.pending) {
             welcomeChannel = await client.channels.fetch(config.welcomeChannel);
-            welcomeChannel.send(`Welcome <@${newMember.toString()}> to the Imagine Dragons server! Feel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself. If you want roles / colored names, type "-rr" for the role selection. We hope you enjoy your stay here!`)
+            welcomeChannel.send(`Welcome ${newMember.toString()} to the Imagine Dragons server! Feel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself. If you want roles / colored names, type "-rr" for the role selection. We hope you enjoy your stay here!`)
         }
     }
 }
