6281e66f5ad16cec6e83c92ccda4c341d08ffcc8
Raz0ra <57400440+Raz0ra@users.noreply.github.com>
2021-10-15 10:40:14 +0200

    Added new EP rerelease songs

--- songvssong.js
+++ songvssong.js
@@ -123,7 +123,10 @@ module.exports = async (client) => {
                 ["Cutthroat", "https://www.youtube.com/watch?v=89xtgEk9jTo"],
                 ["No Time For Toxic People", "https://www.youtube.com/watch?v=wJt2qKPZpsk"],
                 ["One Day", "https://www.youtube.com/watch?v=NzImKCeQnX0"],
-                ["Imagine Dragons (by Ragged Insomnia)", "https://www.youtube.com/watch?v=5hNq_7xtVl0"]];
+                ["Imagine Dragons (by Ragged Insomnia)", "https://www.youtube.com/watch?v=5hNq_7xtVl0"],
+                ["Easy", "https://www.youtube.com/watch?v=fPWzQ_z9VYs"],
+                ["Hole Inside Our Chests", "https://www.youtube.com/watch?v=_NuspI2KPBA"],
+                ["Dolphins", "https://www.youtube.com/watch?v=hcEkPLNNSJo"]];
 
     function randomiser() {
         return Math.floor(Math.random() * songs.length);
