c4c0aa683b6bf7c374f76bff9dce2a3343b3a9f5
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2022-04-02 12:17:23 +0200

    Revert Aprils Fools changes (Ragged Insomnia)

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,8 +2,7 @@ module.exports = async (client, oldMember, newMember) => {
     if (newMember.guild.id == "226576492782551042") {
         if (oldMember.pending && !newMember.pending) {
             welcomeChannel = await client.channels.fetch(config.welcomeChannel);
-            // welcomeChannel.send(`Welcome ${newMember.toString()} to the Imagine Dragons server! Feel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself. If you want roles to represent your favourite album, pronouns, or get notified for events, type "/roles" for the role selection. We hope you enjoy your stay here!`)
-            welcomeChannel.send(`Welcome ${newMember.toString()} to the Ragged Insomnia server! Feel free to introduce yourself to others, you can tell them things like your favourite song, how you found RI, and/or a little about yourself. We hope you enjoy your stay here!`)
+            welcomeChannel.send("**Welcome ${newMember.toString()} to the Imagine Dragons server!**\nFeel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself.\nIf you want roles to represent your favourite album, pronouns, or get notified for events, type ``/roles`` for the **role selection**.\nWe hope you enjoy your stay here!")
         }
     }
 }
