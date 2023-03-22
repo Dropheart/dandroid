d5b8302afd0ff133fe7819c785186c06f8667a89
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2023-03-23 00:42:01 +0100

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,7 +2,7 @@ const { Events } = require('discord.js');
 
 module.exports = {
 	name: Events.GuildMemberUpdate,
-	async execute(oldMember, newMember) {
+	async execute(oldMember, newMember, client) {
         console.log(newMember);
 		if (newMember.guild.id == "226576492782551042") {
             if (oldMember.pending && !newMember.pending) {
@@ -10,5 +10,5 @@ module.exports = {
                 welcomeChannel.send(`**Welcome ${newMember.toString()} to the Imagine Dragons server!**\nFeel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself.\nIf you want roles to represent your favourite album, pronouns, or get notified for events, type \`\`/roles\`\` in any channel for the **role selection**.\nWe hope you enjoy your stay here!`)
             }
         }
-	},
+	}
 };
