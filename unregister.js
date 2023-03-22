1efd2f562dd262f28c92e0b0815e590e43d4f364
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2023-03-22 23:45:28 +0100

    Update unregister.js

--- unregister.js
+++ unregister.js
@@ -3,10 +3,6 @@ const { clientId, guildId, token } = require('./config.json');
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-// Place your client and guild ids here
-const clientId = '818178566037831770';
-const guildId = '226576492782551042';
-
 // for guild-based commands
 rest.delete(Routes.applicationGuildCommand(clientId, guildId, '897552853877530624'))
 	.then(() => console.log('Successfully deleted guild command'))
