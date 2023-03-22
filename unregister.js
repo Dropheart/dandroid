dce88a93396e396169eb7ec108822d7c05cf912f
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2023-03-22 23:52:03 +0100

    Delete unregister.js

--- unregister.js
+++
@@ -1,9 +0,0 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
-
-const rest = new REST({ version: '10' }).setToken(token);
-
-// for guild-based commands
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, '897552853877530624'))
-	.then(() => console.log('Successfully deleted guild command'))
-	.catch(console.error);
