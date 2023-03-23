1cd9e70f149f99bb5019cbcde8bbaf5346abdff1
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2023-03-23 01:01:37 +0100

    Remove suspicious usernames and update user name + discriminator changes

--- events/guildMemberAdd.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client, member) => {
-    if (member.user.username.toLowerCase().includes('h0nde') || member.user.username.toLowerCase().includes('h0nda')) member.ban({ days: 7, reason: 'Automatic ban for h0nde spambot accounts.' });
-    if (member.user.username.toLowerCase().includes('moderator') || member.user.username.toLowerCase().includes('hypesquad')) member.ban({ reason: 'Common Scam Account Name' })
-}
--- events/userUpdate.js
+++ events/userUpdate.js
@@ -1,23 +1,26 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, Events } = require('discord.js');
 
-module.exports = async (client, oldUser, newUser) => {
-    logChannel = await client.channels.fetch("226594976975421441"); // #server-log
+module.exports = {
+	name: Events.UserUpdate,
+	async execute(oldUser, newUser) {
+        logChannel = await newUser.client.channels.fetch("226594976975421441"); // #server-log
 
-    if (oldUser.username !== newUser.username) {
-        usernameembed = new EmbedBuilder()
-                .setColor('ffb347')
-                .setTimestamp()
-                .setThumbnail(newUser.displayAvatarURL())
-                .setDescription(`:pencil: **NICKNAME CHANGE** for <@${newUser.id}>, \`\`${newUser.id}\`\`:\n${oldUser.username} to **${newUser.username}**`);
-                logChannel.send({ embeds: [usernameembed] });
-    }
+        if (oldUser.username !== newUser.username) {
+            usernameembed = new EmbedBuilder()
+                    .setColor('ffb347')
+                    .setTimestamp()
+                    .setThumbnail(newUser.displayAvatarURL())
+                    .setDescription(`:pencil: **NICKNAME CHANGE** for <@${newUser.id}>, \`\`${newUser.id}\`\`:\n${oldUser.username} to **${newUser.username}**`);
+                    logChannel.send({ embeds: [usernameembed] });
+        }
 
-    if (oldUser.discriminator !== newUser.discriminator) {
-        discriminatorembed = new EmbedBuilder()
-                .setColor('ffb347')
-                .setTimestamp()
-                .setThumbnail(newUser.displayAvatarURL())
-                .setDescription(`:pencil: **DISCRIMINATOR CHANGE** for <@${newUser.id}>, \`\`${newUser.id}\`\`:\n${oldUser.username}#${oldUser.discriminator} to **${newUser.username}#${newUser.discriminator}**`);
-                logChannel.send({ embeds: [discriminatorembed] });
-    }
-}
+        if (oldUser.discriminator !== newUser.discriminator) {
+            discriminatorembed = new EmbedBuilder()
+                    .setColor('ffb347')
+                    .setTimestamp()
+                    .setThumbnail(newUser.displayAvatarURL())
+                    .setDescription(`:pencil: **DISCRIMINATOR CHANGE** for <@${newUser.id}>, \`\`${newUser.id}\`\`:\n${oldUser.username}#${oldUser.discriminator} to **${newUser.username}#${newUser.discriminator}**`);
+                    logChannel.send({ embeds: [discriminatorembed] });
+        }
+	}
+};
