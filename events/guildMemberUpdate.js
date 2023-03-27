88b6195758414dafd85fd5c926bf7d45ca7f2ee8
Emilie Sabo <57400440+Raz0ra@users.noreply.github.com>
2023-03-27 07:20:39 +0200

    Update Join Message to reflect role selection changes

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -6,7 +6,7 @@ module.exports = {
 		if (newMember.guild.id == "226576492782551042") {
             if (oldMember.pending && !newMember.pending) {
                 welcomeChannel = await newMember.client.channels.fetch(config.welcomeChannel);
-                welcomeChannel.send(`**Welcome ${newMember.toString()} to the Imagine Dragons server!**\nFeel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself.\nIf you want roles to represent your favourite album, pronouns, or get notified for events, type \`\`/roles\`\` in any channel for the **role selection**.\nWe hope you enjoy your stay here!`)
+                welcomeChannel.send(`**Welcome ${newMember.toString()} to the Imagine Dragons server!**\nFeel free to introduce yourself to others, you can tell them things like your favourite song, how you found ID, and/or a little about yourself.\nIf you want roles to represent your favourite album, pronouns, or get notified for events, click on <id:customize> for the **role selection**.\nWe hope you enjoy your stay here!`)
             }
         }
 	}
